ed258ba850ff64f9ebbe88d89415380f890dbdfb
Andy Harman <andyh.at.pendragon@gmail.com>
2020-05-01 21:11:58 +0100

    Fix warning about slider depth

--- src/ui/Slider/index.js
+++ src/ui/Slider/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Range, Direction } from 'react-range'
 import { Container, SliderTrackRange, SliderThumb } from './elements'
 
-export default React.memo(function Slider({ label, value, onChange, step }) {
-  const resolution = 127
-  const setValue = values => onChange(parseFloat(values[0]) / resolution)
+export default React.memo(function Slider({ label, value, onChange }) {
+  const resolution = 100
+  const setValue = (values) => onChange(parseFloat(values[0]) / resolution)
   return (
     <Container>
       <Range
@@ -12,14 +12,14 @@ export default React.memo(function Slider({ label, value, onChange, step }) {
         max={resolution}
         min={0}
         onChange={setValue}
-        step={step * resolution || 1}
+        step={1}
         renderTrack={({ props, children }) => (
           <SliderTrackRange props={props} label={label}>
             {children}
           </SliderTrackRange>
         )}
         renderThumb={({ props }) => <SliderThumb {...props} />}
-        values={[Math.round(value * 127)]}
+        values={[Math.round(value * resolution)]}
       />
     </Container>
   )
