e7642775f07840bb3d209b41d389329cce97eee8
Andy Harman <andyh.at.pendragon@gmail.com>
2020-05-12 18:05:34 +0100

    Upgrade pseudo-noise to 24bit.

--- src/junox/noise.js
+++ src/junox/noise.js
@@ -1,12 +1,12 @@
 // Pseudo Random Number (PN Sequence) Oscillator
 // Exhibits a spectrum that is between white noise (totally random) and pink noise (roll off at -3db/octave).
 // Based on code from section 5.17 of Pirkle's Synth book.
-// Modified to use 16bit UINT.
+// Modified to use 20bit UINT.
 export class Noise {
   constructor() {
-    this._b15 = 32768
-    this._oneOverB15 = 1.0 / this._b15
-    this._currentBits = 129 | ~~(Math.random() * this._b15)
+    this._b23 = 8388608
+    this._oneOverB23 = 1.0 / this._b23
+    this._currentBits = 129 | ~~(Math.random() * this._b23)
   }
 
   render() {
@@ -19,13 +19,13 @@ export class Noise {
     // Shift 1 bit right.
     this._currentBits >>= 1
 
-    // Add (or not) bit 15.
+    // Add (or not) bit 23.
     const xorBits = b0 ^ b2 ^ b3 ^ b10
     if (xorBits === 1) {
-      this._currentBits |= this._b15
+      this._currentBits |= this._b23
     }
 
     // Convert uint into -1 to +1 range.
-    return this._currentBits * this._oneOverB15 - 1.0
+    return this._currentBits * this._oneOverB23 - 1.0
   }
 }
