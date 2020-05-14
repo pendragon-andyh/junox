9b533befd833fcf53161662fc255e19976d05ede
Andy Harman <andyh.at.pendragon@gmail.com>
2020-05-14 17:35:18 +0100

    Minor tidy of OnePoleFilter

--- src/junox/onepolefilter.js
+++ src/junox/onepolefilter.js
@@ -2,21 +2,15 @@
 export class OnePoleFilter {
   constructor(piOverSampleRate) {
     this.piOverSampleRate = piOverSampleRate
+
     this.beta = 0.0
-    this.z1 = 0.0
     this.gamma = 1.0
     this.delta = 0.0
     this.epsilon = 0.0
     this.a0 = 1.0
-    this.feedback = 0.0
-  }
 
-  setFeedback(feedback) {
-    this.feedback = feedback
-  }
-
-  feedbackOutput() {
-    return this.beta * (this.z1 + this.feedback * this.delta)
+    this._z1 = 0.0
+    this._feedback = 0.0
   }
 
   setCutoff(cutoff) {
@@ -24,17 +18,24 @@ export class OnePoleFilter {
     this.alpha = g / (1.0 + g)
   }
 
+  setFeedback(feedback) {
+    this._feedback = feedback
+  }
+
+  feedbackOutput() {
+    return this.beta * (this._z1 + this._feedback * this.delta)
+  }
+
   reset() {
-    this.z1 = 0.0
-    this.feedback = 0.0
+    this._z1 = 0.0
+    this._feedback = 0.0
   }
 
   render(input) {
-    const xIn =
-      input * this.gamma + this.feedback + this.epsilon * this.feedbackOutput()
-    const vn = (this.a0 * xIn - this.z1) * this.alpha
-    const out = vn + this.z1
-    this.z1 = vn + out
+    const xIn = input * this.gamma + this._feedback + this.epsilon * this.feedbackOutput()
+    const vn = (this.a0 * xIn - this._z1) * this.alpha
+    const out = vn + this._z1
+    this._z1 = vn + out
     return out
   }
 }
