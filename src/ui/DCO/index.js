6b55f6af78d6436e17d845c0a627d6f695a6fdd6
Andy Harman <andyh.at.pendragon@gmail.com>
2020-05-24 23:46:07 +0100

    Correct order of DCO range buttons

--- src/ui/DCO/index.js
+++ src/ui/DCO/index.js
@@ -17,39 +17,15 @@ export default React.memo(function DCO({
 }) {
   return (
     <Section title="DCO">
-      <ButtonLED
-        active={range === 2}
-        label="4'"
-        toggle={setValue('dco.range', 2)}
-      />
-      <ButtonLED
-        active={range === 1}
-        label="8'"
-        toggle={setValue('dco.range', 1)}
-      />
-      <ButtonLED
-        active={range === 0.5}
-        label="16'"
-        toggle={setValue('dco.range', 0.5)}
-      />
+      <ButtonLED active={range === 0.5} label="16'" toggle={setValue('dco.range', 0.5)} />
+      <ButtonLED active={range === 1} label="8'" toggle={setValue('dco.range', 1)} />
+      <ButtonLED active={range === 2} label="4'" toggle={setValue('dco.range', 2)} />
       <AfterButtonLED />
       <Slider label="LFO" value={lfo} onChange={setValue('dco.lfo')} />
       <Slider label="PWM" value={pwm} onChange={setValue('dco.pwm')} />
-      <ButtonLED
-        active={pwmMod === 'l'}
-        label="LFO"
-        toggle={setValue('dco.pwmMod', 'l')}
-      />
-      <ButtonLED
-        active={pwmMod === 'm'}
-        label="Man"
-        toggle={setValue('dco.pwmMod', 'm')}
-      />
-      <ButtonLED
-        active={pwmMod === 'e'}
-        label="Env"
-        toggle={setValue('dco.pwmMod', 'e')}
-      />
+      <ButtonLED active={pwmMod === 'l'} label="LFO" toggle={setValue('dco.pwmMod', 'l')} />
+      <ButtonLED active={pwmMod === 'm'} label="Man" toggle={setValue('dco.pwmMod', 'm')} />
+      <ButtonLED active={pwmMod === 'e'} label="Env" toggle={setValue('dco.pwmMod', 'e')} />
       <AfterButtonLED />
 
       <ButtonLED
@@ -59,26 +35,10 @@ export default React.memo(function DCO({
         toggle={setValue('dco.pulse')}
         variant="white"
       />
-      <ButtonLED
-        active={saw}
-        label="SAW"
-        spaced
-        toggle={setValue('dco.saw')}
-        variant="yellow"
-      />
-      <ButtonLED
-        active={sub}
-        label="SUB"
-        spaced
-        toggle={setValue('dco.sub')}
-        variant="orange"
-      />
+      <ButtonLED active={saw} label="SAW" spaced toggle={setValue('dco.saw')} variant="yellow" />
+      <ButtonLED active={sub} label="SUB" spaced toggle={setValue('dco.sub')} variant="orange" />
       <AfterButtonLED />
-      <Slider
-        label="SUB"
-        onChange={setValue('dco.subAmount')}
-        value={subAmount}
-      />
+      <Slider label="SUB" onChange={setValue('dco.subAmount')} value={subAmount} />
       <Slider label="NOISE" value={noise} onChange={setValue('dco.noise')} />
     </Section>
   )
