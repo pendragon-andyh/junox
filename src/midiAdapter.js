39f12c3798d36385c0b1b9d10922cf87eaf95de0
Andy Harman <andyh.at.pendragon@gmail.com>
2020-06-04 15:26:31 +0100

    Updates

--- src/midiAdapter.js
+++ src/midiAdapter.js
@@ -1,17 +1,31 @@
 export class MidiAdapter {
   _midiAccess = null
-  _midiOptions = {}
 
-  withSysex(sysexMode = true) {
-    this._midiOptions.sysex = sysexMode
-    return this
-  }
+  /**
+   * @property - Currently-selected MIDI input port.
+   * @type {MIDIInput}
+   */
+  inputPort = null
 
+  /**
+   * @property - Currently-selected consumer of MIDI inputs.
+   * @type {MidiConsumer}
+   */
+  inputConsumer = null
+
+  /**
+   * Returns True if the instance is currently connected to MIDI.
+   */
   isConnected() {
     return this._midiAccess !== null
   }
 
-  async start(onStateChange = null) {
+  /**
+   * Connect to MIDI.
+   * @param {Object} midiOptions - Options object (e.g. to specify Sysex).
+   * @param {Function} onStateChange - Optional event-handler function for MIDI state changes.
+   */
+  async connect(midiOptions = {}, onStateChange = null) {
     if (navigator.requestMIDIAccess) {
       this._midiAccess = null
 
@@ -25,7 +39,7 @@ export class MidiAdapter {
         console.log('MIDI access failed.')
       }
 
-      await navigator.requestMIDIAccess(this._midiOptions).then(midiAccessSuccess, midiAccessFailed)
+      await navigator.requestMIDIAccess(midiOptions).then(midiAccessSuccess, midiAccessFailed)
     } else {
       console.log('MIDI is not available for this browser.')
     }
@@ -33,248 +47,100 @@ export class MidiAdapter {
     return this
   }
 
-  stop() {
+  /**
+   * Disconnect from MIDI.
+   */
+  disconnect() {
     this._midiAccess = null
   }
 
+  /**
+   * Get a list of the currently-available MIDI input ports.
+   */
   listInputs() {
     const results = []
     if (this._midiAccess) {
       this._midiAccess.inputs.forEach((port) => {
-        results.push({ id: port.id, name: port.name, port })
+        const titleCaseName = port.name.charAt(0).toUpperCase() + port.name.slice(1)
+        results.push({ id: port.id, name: titleCaseName, port })
       })
     }
     return results
   }
 
+  /**
+   * Get a list of the currently-available MIDI input ports.
+   */
   listOutputs() {
     const results = []
     if (this._midiAccess) {
       this._midiAccess.outputs.forEach((port) => {
-        results.push({ id: port.id, name: port.name, port })
+        const titleCaseName = port.name.charAt(0).toUpperCase() + port.name.slice(1)
+        results.push({ id: port.id, name: titleCaseName, port })
       })
     }
     return results
   }
 
-  connectFromInput(portId, consumer) {
+  /**
+   * Set the current MIDI input port.
+   * @param {string} portId - Id of the port (from the ```listInputs()``` method)
+   */
+  setInputPort(portId) {
     this.ensureMidiStarted()
 
-    const port = this._midiAccess.inputs.get(portId)
-    if (!port) {
-      throw new RangeError(`MIDI Input port "${portId} not found`)
+    let port = null
+    if (portId) {
+      port = this._midiAccess.inputs.get(portId)
+      if (!port) {
+        throw new RangeError(`MIDI Input port "${portId} not found`)
+      }
     }
 
     // If port is already connected to something then send "all notes off" message to tidy-up.
-    if (port.onmidimessage) {
-      port.onmidimessage([176, 123, 0])
+    if (this.inputPort) {
+      if (this.inputPort.onmidimessage) {
+        this.handleMidiInput([176, 123, 0])
+        this.inputPort.onmidimessage = null
+      }
     }
 
-    // Forward the MIDI event data to the consumer.
-    port.onmidimessage = (ev) => {
-      consumer && consumer.handleMidiInputMessage(ev.data)
+    // Start listening to the new MIDI input port.
+    if (port) {
+      port.onmidimessage = (ev) => this.handleMidiInput(ev.data)
     }
+
+    this.inputPort = port
   }
 
-  disconnectFromInput(portId) {
+  /**
+   * Specify a "consumer" for incoming MIDI messages.
+   * @param {MidiConsumer} consumer - Object htat wants to listen to incoming MIDI input messages.
+   */
+  routeInputMessagesTo(consumer) {
     this.ensureMidiStarted()
-    this.connectFromInput(portId, null)
+
+    if (this.inputConsumer) {
+      this.handleMidiInput([176, 123, 0])
+    }
+
+    this.inputConsumer = consumer
   }
 
+  /**
+   * @private - Forward the MIDI event data to the consumer.
+   * @param {Array<int>} data - MIDI message data.
+   */
+  handleMidiInput(data) {
+    this.inputConsumer && this.inputConsumer.handleMidiInputMessage(data)
+  }
+
+  /**
+   * @private - Confirm that we are currently connected to MIDI.
+   */
   ensureMidiStarted() {
     if (!this._midiAccess) {
       throw new Error(`MIDI connection has not been started`)
-    }
-  }
-}
-
-/**
- * Class that can act as a consumer of MIDI/keyboard inputs.
- */
-export class MidiConsumer {
-  /**
-   * @property  Event handler-method for note-on events.
-   * @type {function} - onNoteOn(noteNumber, velocity)
-   */
-  onNoteOn = null
-
-  /**
-   * @property  Event handler-method for note-off events.
-   * @type {function} - onNoteOff(noteNumber, velocity)
-   */
-  onNoteOff = null
-
-  /**
-   * @property  Event handler-method for control-change (CC) events.
-   * @type {function} - onControlChange(controlNumber, controlValue)
-   */
-  onControlChange = null
-
-  /**
-   * @property  Event handler-method for pitch-bend events.
-   * @type {function} - onPitchBend(bendValue)
-   */
-  onPitchBend = null
-
-  /**
-   * @property Event handler-method for "all notes off" events".
-   * @type {function} - onWatchAll()
-   */
-  onAllNotesOff = null
-
-  /**
-   * @property Event handler-method for watching all incoming-inputs on a MIDI port.
-   * @type {function} - onWatchAll(data)
-   */
-  onWatchAll = null
-
-  /** @private Map of all notes that are currently on */
-  _onNotes = {}
-
-  /** @private Map of all notes that are currently been held by the sustain pedal */
-  _sustainedNotes = {}
-
-  /** @private True if the sustain pedal is active */
-  _isSustained = false
-
-  /** @private Map of CC handler methods for specific controllers */
-  _controlChangeHandlers = {
-    64: function (controlValue) {
-      if (controlValue) {
-        this.sustainOn()
-      } else {
-        this.sustainOff()
-      }
-    },
-    123: this.allNotesOff,
-  }
-
-  /**
-   * Register handler-functions for specific CC event.
-   * @param {number} controlNumber - Control number (between 0 and 127),
-   * @param {number} handler - Handler-function for handling the specified control-change.
-   */
-  registerControlChangeHandler(controlNumber, handler) {
-    this._sustainedNotes[controlNumber] = handler
-  }
-
-  /**
-   * Trigger a note.
-   * @param {number} noteNumber - MIDI note number (0 - 127).
-   * @param {number} velocity - Velocity (1 = soft, 127 = hard, 0 = off).
-   */
-  noteOn(noteNumber, velocity = 64) {
-    // If the velocity is zero then its really a noteOff event.
-    if (velocity === 0) {
-      this.noteOff(noteNumber, 64)
-      return
-    }
-
-    // Invoke the "onNoteOn" event handler to do the real work.
-    this.onNoteOn && this.onNoteOn(noteNumber, velocity)
-
-    // Keep track of the triggered notes.
-    this._onNotes[noteNumber] = true
-    delete this._sustainedNotes[noteNumber]
-  }
-
-  /**
-   * Release a note.
-   * @param {number} noteNumber - MIDI note number (0 - 127).
-   * @param {number} velocity - Velocity (0 = soft, 127 = hard).
-   */
-  noteOff(noteNumber, velocity = 64) {
-    // If the sustain-pedal is active then store -up the note-off event for later.
-    if (this._isSustained) {
-      this._sustainedNotes[noteNumber] = true
-      return
-    }
-
-    // Invoke the "onNoteOff" event handler to do the real work.
-    this.onNoteOff && this.onNoteOff(noteNumber, velocity)
-
-    // Keep track of the triggered notes.
-    delete this._onNotes[noteNumber]
-    delete this._sustainedNotes[noteNumber]
-  }
-
-  /**
-   * Activate the sustain pedal (any released notes will be held until the pedal is deactivated).
-   */
-  sustainOn() {
-    this._isSustained = true
-  }
-
-  /**
-   * Deactivate the sustain-pedal (and release any notes that have been held by it).
-   */
-  sustainOff() {
-    this._isSustained = false
-    for (let sustainedNoteNumber in Object.keys(this._sustainedNotes)) {
-      this.noteOff(sustainedNoteNumber, 64)
-    }
-  }
-
-  /**
-   * Set the value for a control.
-   * @param {number} controlNumber - Control number (0 - 127).
-   * @param {*} controlValue - Control value (0 - 127).
-   */
-  controlChange(controlNumber, controlValue) {
-    // Invoke a specific control-handler if one has been registered.
-    const handler = this._controlChangeHandlers[controlNumber]
-    handler && handler.call(this, controlValue)
-
-    // Invoke the "onControlChange" event handler to do the real work.
-    this.onControlChange && this.onControlChange(controlValue)
-  }
-
-  /**
-   * Set the current pitch-bend value.
-   * @param {number} bendValue - The current value of the bend-lever (-1 to +1).
-   */
-  pitchBend(bendValue) {
-    // Invoke the "onControlChange" event handler to do the real work.
-    this.onPitchBend && this.onPitchBend(bendValue)
-  }
-
-  /**
-   * Release all notes.
-   */
-  allNotesOff() {
-    this._isSustained = false
-    for (let noteNumber in Object.keys(this._onNotes)) {
-      this.noteOff(noteNumber, 64)
-    }
-    this.onAllNotesOff && this.onAllNotesOff()
-  }
-
-  /**
-   * Handle incoming MIDI input events.
-   * @param {Array<int>} data - Incoming event data from the MIDI adapter.
-   */
-  handleMidiInputMessage(data) {
-    // If "watch all" handler has been specified then invoke it.
-    this.onWatchAll && this.onWatchAll(data)
-
-    // Decode the MIDI message and invoke the appropriate behaviour.
-    const messageType = data[0] >> 4
-    switch (messageType) {
-      case 8:
-        this.noteOff(data[1], data[2])
-        break
-      case 9:
-        this.noteOn(data[1], data[2])
-        break
-      case 11:
-        this.controlChange(data[1], data[2])
-        break
-      case 14:
-        const bendValue = ((data[2] << 7) + data[1]) / 8192 - 1
-        this.pitchBend(bendValue)
-        break
-      default:
-        break
     }
   }
 }
